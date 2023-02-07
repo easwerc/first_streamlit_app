168b87a9da358b406326b4b4df667a669676181d
easwerc <easwerc@gmail.com>
2023-02-07 08:51:30 -0500

    Added smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avacoda Toast')
+
+streamlit.text('🍌🍓Build Your Own Fruit Smoothie 🍉🍊')
