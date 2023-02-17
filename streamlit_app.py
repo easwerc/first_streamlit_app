0639c7ad347b58cac35f76857a631c0b86b28dbf
easwerc <easwerc@gmail.com>
2023-02-16 22:12:53 -0500

    organizing code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
+import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My first streamlit app')
 
@@ -27,6 +29,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+streamlit.stop()
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
